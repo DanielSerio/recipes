1a44f9f77ee9a74480282937ee9ad3a50a22c5d9
DanielSerio <dserio.d@gmail.com>
2023-06-25 10:17:09 -0400

    added paging current page input

--- src/lib/components/ApiTable/ApiTablePaging.svelte
+++ src/lib/components/ApiTable/ApiTablePaging.svelte
@@ -6,16 +6,26 @@
 	export let availableLimits: number[];
 	export let totalPages: number;
 	export let totalRecords: number;
+	let currentPageInputValue: number = Math.ceil(
+		($pagingStore.offset + $pagingStore.limit) / $pagingStore.limit
+	);
 
 	$: currentPage = ($pagingStore.offset + $pagingStore.limit) / $pagingStore.limit;
 	$: isLastPage = currentPage === totalPages;
 	$: isFirstPage = currentPage === 1;
+
+	function setCurrentPageInputValue() {
+		currentPageInputValue = Math.ceil(
+			($pagingStore.offset + $pagingStore.limit) / $pagingStore.limit
+		);
+	}
 
 	async function gotToFirstPage() {
 		$pagingStore = {
 			...$pagingStore,
 			offset: 0
 		};
+		setCurrentPageInputValue();
 		await refresh();
 	}
 	async function gotToLastPage() {
@@ -24,6 +34,7 @@
 			...$pagingStore,
 			offset: lastPageOffset
 		};
+		setCurrentPageInputValue();
 		await refresh();
 	}
 
@@ -33,6 +44,7 @@
 			...$pagingStore,
 			offset: nextPageOffset
 		};
+		setCurrentPageInputValue();
 		await refresh();
 	}
 
@@ -42,16 +54,31 @@
 			...$pagingStore,
 			offset: prevPageOffset
 		};
+		setCurrentPageInputValue();
 		await refresh();
 	}
+	const handleInputChange = (e: Event) => {
+		const target = e.target as HTMLInputElement;
+		const value = `${target.value}`.trim();
+
+		if (isNaN(+value) || +value < 1 || +value > totalPages) {
+			setCurrentPageInputValue();
+		} else {
+			const newOffset = (+value - 1) * $pagingStore.limit;
+			$pagingStore = {
+				...$pagingStore,
+				offset: newOffset
+			};
+			refresh();
+		}
+	};
 </script>
 
 <div class="api-table-paging">
 	<div class="records-area">
 		<span class="text"
-			>{totalRecords} record{totalRecords === 1 ? '' : 's'} in {totalPages} page{totalPages === 1
-				? ''
-				: 's'} of</span
+			><b>{totalRecords}</b> record{totalRecords === 1 ? '' : 's'} in <b>{totalPages}</b>
+			page{totalPages === 1 ? '' : 's'} of</span
 		>
 		<select bind:value={$pagingStore.limit} on:change={refresh}>
 			{#each availableLimits as limitOption}
@@ -68,7 +95,15 @@
 				<i class="bi bi-chevron-left" />
 			</button>
 		</div>
-		{Math.ceil(($pagingStore.offset + $pagingStore.limit) / $pagingStore.limit)} / {totalPages}
+		<div class="page-area">
+			<input
+				class="page-input"
+				type="number"
+				bind:value={currentPageInputValue}
+				on:change={handleInputChange}
+			/>
+			<span class="muted">/ {totalPages}</span>
+		</div>
 		<div class="btn-group">
 			<button disabled={isLastPage} on:click={gotToNextPage}>
 				<i class="bi bi-chevron-right" />
